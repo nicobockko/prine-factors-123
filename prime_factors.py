dca5920699064b57e975ce0c746b555d4c719d5a
junsik <junsik@ma.il>
2025-07-31 11:39:55 +0900

    [feature] p.54 완료

--- prime_factors.py
+++ prime_factors.py
@@ -1,20 +1,13 @@
 class PrimeFactor:
     def of(self, number) -> []:
         factors = []
-        if number > 1:
-            devisor = 2
-            if number == 4:
-                while number % devisor == 0:
-                    factors.append(devisor)
-                    number //= devisor
-            elif number == 6:
-                while number % devisor == 0:
-                    factors.append(devisor)
-                    number //= devisor
-                devisor+=1
-                while number % devisor == 0:
-                    factors.append(devisor)
-                    number //= devisor
-            else:
-                factors.append(number)
+        devisor = 2
+        while number % devisor == 0:
+            factors.append(devisor)
+            number //= devisor
+        devisor += 1
+        while number % devisor == 0:
+            factors.append(devisor)
+            number //= devisor
+
         return factors
