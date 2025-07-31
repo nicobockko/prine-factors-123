22d1d40613b24d7a7fbdb2ec385a7d507c770003
junsik <junsik@ma.il>
2025-07-31 11:24:25 +0900

    [refector] p.41  while반영

--- prime_factors.py
+++ prime_factors.py
@@ -1,10 +1,11 @@
 class PrimeFactor:
     def of(self, number) -> []:
         factors = []
-        if number >1:
-            if number ==4:
-                factors.append(2)
-                factors.append(2)
+        if number > 1:
+            if number == 4:
+                while number % 2 == 0:
+                    factors.append(2)
+                    number //= 2
             else:
                 factors.append(number)
 
