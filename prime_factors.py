77b7fafb38594dbc972f1309e391947dbf8c230f
junsik <junsik@ma.il>
2025-07-31 11:35:32 +0900

    [refactor] p.46  소인수 변수화

--- prime_factors.py
+++ prime_factors.py
@@ -2,13 +2,19 @@ class PrimeFactor:
     def of(self, number) -> []:
         factors = []
         if number > 1:
+            devisor = 2
             if number == 4:
-                while number % 2 == 0:
-                    factors.append(2)
-                    number //= 2
+                while number % devisor == 0:
+                    factors.append(devisor)
+                    number //= devisor
             elif number == 6:
-                factors.append(2)
-                factors.append(3)
+                while number % devisor == 0:
+                    factors.append(devisor)
+                    number //= devisor
+                devisor+=1
+                while number % devisor == 0:
+                    factors.append(devisor)
+                    number //= devisor
             else:
                 factors.append(number)
         return factors
